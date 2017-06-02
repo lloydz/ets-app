c83e0092da19dc755c6f64012abd66ed4eaed44a
zhhj <zhhjchxy@163.com>
2017-06-02 17:58:51 +0800

    ------------

--- resources/app/index.js
+++ resources/app/index.js
@@ -4,30 +4,44 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win = null
+
+const alreadyOpened = app.makeSingleInstance((commandLine, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (win) {
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+// if (alreadyOpened) {
+//   app.quit()
+// }
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 800})
+  if(!alreadyOpened) {
+    // Create the browser window.
+    win = new BrowserWindow({width: 1200, height: 800})
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  // win.loadURL('https://www.baidu.com/')
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+    // win.loadURL('https://www.baidu.com/')
 
-  // Open the DevTools.
-  // win.webContents.openDevTools()
+    // Open the DevTools.
+    // win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
+  }
 }
 
 // This method will be called when Electron has finished
